ab266d22028b92f490518227ea043a7eeabcd8a0
Slater-Victoroff <slater.victoroff@students.olin.edu>
2013-01-18 23:33:00 -0500

    First commit, basic rails starting app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SustainableResource::Application.config.secret_token = '7acec9ea9440d245866494b9a5eff4ce77e9999d7faa625623b6df03e153c72f9813b3f79d9616c885bd43b0f5dc2db67f6e8d8799e30a23daed277ba6577534'
